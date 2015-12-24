d73f5e709840dca971aea29084362e3b82bcd52a
Nick Gridinskiy <nickgri@sparkcentral.com>
2015-12-23 16:36:52 -0800

    Initial commit

---
+++ components/channels/Channel.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class Channel extends React.Component {
+
+  onClick(e) {
+    e.preventDefault();
+    const {setChannel, channel} = this.props;
+    setChannel(channel);
+  }
+
+  render() {
+    const {channel} = this.props;
+    return (
+      <li>
+        <a onClick = {this.onClick.bind(this)}>
+          {channel.name}
+        </a>
+      </li>
+    );
+  }
+
+}
+
+Channel.propTypes = {
+  channel: React.PropTypes.object.isRequired,
+  setChannel: React.PropTypes.func.isRequired
+}
+
+export default Channel;
